da83c9f27834cbb89ee8a40b0e52425e72acbd61
mecheline <ucheamaechi2016@gmail.com>
2023-03-22 20:00:35 +0100

    18th commit

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "/api/api-handler",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
