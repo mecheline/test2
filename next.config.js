e83ede144bcde6702a29ca74c4d9767fd715495b
mecheline <ucheamaechi2016@gmail.com>
2023-03-22 23:48:45 +0100

    19th commit

--- next.config.js
+++ next.config.js
@@ -1,35 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // async headers() {
-  //   return [
-  //     {
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 
-  //       source: "/api/api-handler",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Credentials", value: "true" },
-  //         { key: "Access-Control-Allow-Origin", value: "*" },
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-  //         },
-  //         {
-  //           key: "Access-Control-Allow-Headers",
-  //           value:
-  //             "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
-  async rewrites() {
+module.exports = () => {
+  const rewrites = () => {
     return [
       {
-        source: "/api/:path*",
-        destination: "https://kunda-test2.vercel.app/:path",
+        source: "/api/api-handler",
+        destination: "https://kunda-test2.vercel.app/api/api-handler",
       },
     ];
-  },
+  };
+  return {
+    rewrites,
+  };
 };
-
-module.exports = nextConfig;
