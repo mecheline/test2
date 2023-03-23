ab1cfe470da52d64db829fe3795af577359e755e
mecheline <ucheamaechi2016@gmail.com>
2023-03-23 06:53:51 +0100

    23th commit

--- pages/index.js
+++ pages/index.js
@@ -10,22 +10,19 @@ export default function Home() {
 }
 
 export async function getServerSideProps() {
-  // const response = await fetch(
-  //   "https://kunda-test2.vercel.app/api/api-handler",
-  //   {
-  //     method: "GET",
-  //   }
-  // );
-  // const lists = await response.json();
-  // console.log(lists);
+  let a = false;
+  if (a) {
+    return {
+      redirect: {
+        destination: "https://kunda-test2.vercel.app/",
+        permanent: false,
+      },
+    };
+  }
 
   return {
-    // props: {
-    //   lists: lists ? { lists } : "",
-    // },
-    redirect: {
-      destination: "https://kunda-test2.vercel.app/",
-      permanent: false,
+    props: {
+      data: "",
     },
   };
 }
